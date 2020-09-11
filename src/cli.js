4ca649e3639a908e13923e54fa870816e057c71a
Thim Lohse <thim.lohse@klarna.com>
2020-09-11 13:30:10 +0200

    changed export type of default function

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import minimist from "minimist";
 
 const parseArgs = (rawArgs) => minimist(rawArgs.slice(2));
-export default async (rawArgs) => {
+
+export async function cli(rawArgs) {
   const args = parseArgs(rawArgs);
 
   console.log("rawArgs", rawArgs);
   console.log("args", args);
-};
+}
